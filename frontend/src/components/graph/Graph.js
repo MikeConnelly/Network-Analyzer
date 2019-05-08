5cb74a896386a457a5553fa8a1840c2ff3f23a53
Michael Connelly <mrc33@atlanticbb.net>
2019-05-07 22:42:52 -0400

    time filter menu

--- frontend/src/components/graph/Graph.js
+++ frontend/src/components/graph/Graph.js
@@ -1,22 +1,27 @@
 import React, {Component} from 'react';
 import {AreaChart, XAxis, YAxis, CartesianGrid, Tooltip, Area} from 'recharts';
+import FilterButton from './filter-button/FilterButton';
 
+// check out material-ui line chart
 class Graph extends Component {
   render() {
     return (
-      <AreaChart 
-        width={400}
-        height={400}
-        data={this.props.data}
-        margin={{ top: 20, right: 30, left: 0, bottom: 0}}
-      >
-        <XAxis dataKey="dateObject.xaxis" />
-        <YAxis />
-        <CartesianGrid strokeDasharray="3 3" />
-        <Tooltip />
-        <Area type="monotone" dataKey="speeds.download" stroke="#8884d8" fill="#8884d8" />
-        <Area type="monotone" dataKey="speeds.upload" stroke="#00dd55" fill="#00dd55" />
-      </AreaChart>
+      <div class="graph">
+        <AreaChart 
+          width={800}
+          height={400}
+          data={this.props.data}
+          margin={{ top: 20, right: 30, left: 0, bottom: 0}}
+        >
+          <XAxis dataKey="dateObject.xaxis" />
+          <YAxis />
+          <CartesianGrid strokeDasharray="3 3" />
+          <Tooltip />
+          <Area type="monotone" dataKey="speeds.download" stroke="#8884d8" fill="#8884d8" />
+          <Area type="monotone" dataKey="speeds.upload" stroke="#00dd55" fill="#00dd55" />
+        </AreaChart>
+        <FilterButton actions={this.props.actions} />
+      </div>
     )
   }
 }
---
+++ frontend/src/components/graph/filter-button/FilterButton.js
@@ -0,0 +1,66 @@
+import React, {Component} from 'react';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+
+class FilterButton extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      anchorEl: null
+    };
+    this.getPastTime = this.getPastTime.bind(this);
+  }
+
+  getPastTime = timeId => {
+    const now = new Date();
+    const twentyFourHourAgoTime = now.setDate(now.getDate() - 1);
+    switch(timeId) {
+      case 1:
+        return twentyFourHourAgoTime;
+      case 2:
+        const lastWeekTime = now.setDate(now.getDate() - 7);
+        return lastWeekTime;
+      default:
+        return twentyFourHourAgoTime; 
+    }
+  }
+
+  handleClick = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  }
+
+  handleClose = (event, timeId) => {
+    this.setState({ anchorEl: null });
+    const pastTime = this.getPastTime(timeId);
+    this.props.actions.getRecent(pastTime, Date.now());
+  }
+
+  render() {
+    const {anchorEl} = this.state;
+    console.log(anchorEl)
+
+    return (
+      <div>
+        <Button
+          aria-owns={anchorEl ? 'simple-menu' : undefined}
+          aria-haspopup="true"
+          onClick={this.handleClick}
+        >
+          Timeframe
+        </Button>
+        <Menu
+          id="simple-menu"
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={this.handleClose}
+        >
+          <MenuItem onClick={(e) => this.handleClose(e, 1)}>Past 24 hours</MenuItem>
+          <MenuItem onClick={(e) => this.handleClose(e, 2)}>Past week</MenuItem>
+        </Menu>
+      </div>
+    );
+  }
+}
+
+export default FilterButton;
